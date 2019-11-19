9eab2ab5dcfb42d2f4ddf926991f2d40159d14f9
RubensSoft <joserubencastrosoriano@gmail.com>
2019-11-19 10:50:45 +0100

    closed #1 Create class CustomCell

---
+++ SuperHeroesApp/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  SuperHeroesApp
+//
+//  Created by Castro, Jose Rubén on 19/11/2019.
+//  Copyright © 2019 Galera, Javier. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell : UITableViewCell {
+    
+    @IBOutlet weak var superHeroNameLabel: UILabel!
+    @IBOutlet weak var superHeroImage: UIImageView!
+    
+}
